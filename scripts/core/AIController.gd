e16b7c1e0d1294aa6671fefa76bc75216f101030
Goh60H <136587134+Goh60H@users.noreply.github.com>
2026-02-09 22:07:06 +0100

    Fixed original AI controller and added a new black hole effect for the classical character special. Changed the background

--- scripts/core/AIController.gd
+++ scripts/core/AIController.gd
@@ -1,14 +1,17 @@
 extends Node
 
-enum E_Mood {ESCAPING, HUNTING, TESTING}
+enum E_Mood {ESCAPING, HUNTING}
 
 @onready var starting_timer: Timer = $StartingTime
 
 var enemy : CharacterParent
 var controlledCharacter : CharacterParent
 
-var mood : E_Mood = E_Mood.TESTING
+var mood : E_Mood = E_Mood.HUNTING
 var time_since_last_jump : float = 0
+
+
+var time_since_last_action : float = 0
 
 var isSetUp = false
 
@@ -25,6 +28,12 @@ func setup(inEnemy : CharacterParent, inControlledCharacter : CharacterParent) -
 
 
 func _physics_process(delta: float) -> void:
+	if 0.2 < time_since_last_action:
+		time_since_last_action = 0
+	else:
+		time_since_last_action += delta
+		return
+	
 	if not isSetUp:
 		return
 	if not controlledCharacter or not is_instance_valid(controlledCharacter): return
@@ -35,35 +44,22 @@ func _physics_process(delta: float) -> void:
 		mood = E_Mood.HUNTING
 	if randf() < time_since_last_jump:
 		jump()
+		time_since_last_jump = 0
+	else:
+		time_since_last_jump += delta
 	match mood:
 		# Prioritizes surviving
 		E_Mood.ESCAPING:
 			move_away_from_enemy()
 			if randf() < 0.1:
-				look_towards_enemy()
 				use_ranged_attack()
 		# Prioritizes dealing damage
 		E_Mood.HUNTING:
-			look_towards_enemy()
-			use_ranged_attack()
-			if im_in_melee_range():
-				look_towards_enemy()
-				use_light_attack()
-			look_towards_enemy()
-			use_ranged_attack()
-			move_towards_enemy()
-		# Balanced between dealing damage and surviving
-		E_Mood.TESTING:
-			if im_in_melee_range():
-				look_towards_enemy()
-				use_light_attack()
-			if randf() < 0.5:
-				move_away_from_enemy()
-			else:
-				move_towards_enemy()
-			if randf() < 0.5:
-				look_towards_enemy()
+			if randf() < 0.2:
 				use_ranged_attack()
+			if im_in_melee_range():
+				use_light_attack()
+			move_towards_enemy()
 
 
 func im_in_melee_range() -> bool:
@@ -82,11 +78,9 @@ func look_away_from_enemy() -> void:
 
 func look_towards_enemy() -> void:
 	if im_on_the_right():
-		Input.action_press("move_left_2")
-		Input.action_release("move_left_2")
+		safe_random_yielding_input_pressing("move_left_2", 1)
 	else:
-		Input.action_press("move_right_2")
-		Input.action_release("move_right_2")
+		safe_random_yielding_input_pressing("move_right_2", 1)
 
 func move_away_from_enemy() -> void:
 	if im_on_the_right():
@@ -101,22 +95,28 @@ func move_towards_enemy() -> void:
 		safe_random_yielding_input_pressing("move_right_2")
 
 
-func safe_random_yielding_input_pressing(input_name: String):
-	var timer = get_tree().create_timer(randf())
+func safe_random_yielding_input_pressing(input_name: String, customTime : float = 0):
+	var timer = get_tree().create_timer(randf()*4 if customTime==0 else customTime)
 	timer.timeout.connect(func(): Input.action_release(input_name))
 	Input.action_press(input_name)
 
 func use_light_attack() -> void:
 	Input.action_press("light_attack_2")
 	Input.action_release("light_attack_2")
+	look_towards_enemy()
+	
+	
+	
 
 func use_ranged_attack() -> void:
 	Input.action_press("ranged_attack_2")
 	Input.action_release("ranged_attack_2")
+	look_towards_enemy()
 
 func use_especial_attack() -> void:
 	Input.action_press("especial_attack_2")
 	Input.action_release("especial_attack_2")
+	look_towards_enemy()
 
 func jump() -> void:
 	Input.action_press("jump_2")
