4f2dff79a10ae8472deebe8eadba0d35d667fe39
Goh60H <136587134+Goh60H@users.noreply.github.com>
2026-02-10 14:05:53 +0100

    Character selection panel completed. Now working on the script

---
+++ scripts/ui/character_selection.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+
+@onready var swapRight1 : Button = $ButtonR1
+@onready var swapRight2: Button = $ButtonR2
+@onready var swapLeft1 : Button = $ButtonL1
+@onready var swapLeft2: Button = $ButtonL1
+@onready var playButton : Button = $ButtonPlay
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
