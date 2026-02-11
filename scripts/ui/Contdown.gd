c03184a120994ff1cdc526a0b5a0a4a44d9fd63d
Kentalives <bruno.fdez.official@gmail.com>
2026-02-11 10:43:11 +0100

    Added sound to starting countdown

--- scripts/ui/Contdown.gd
+++ scripts/ui/Contdown.gd
@@ -1,8 +1,15 @@
 extends CanvasLayer
 
+@onready var sound: AudioStreamPlayer = $Sound
+@onready var panel: Panel = $Panel
+
 func start_countdown() -> void:
-	for i in range(3, 0, -1):
+	panel.visible = true
+	AudioManager.play_sound_safe(sound)
+	
+	for i in range(2, 0, -1):
 		$Label.text = str(i)
 		await get_tree().create_timer(1.0).timeout
 	$Label.text = "GO!"
+	panel.visible = false
 	await get_tree().create_timer(0.5).timeout
