92149aab25c29e9461fcbfc31917fb33aec76cc8
Goh60H <136587134+Goh60H@users.noreply.github.com>
2026-02-12 17:00:36 +0100

    Changed GrenMap name and started developing it

--- scripts/ui/map_selector.gd
+++ scripts/ui/map_selector.gd
@@ -25,7 +25,7 @@ var MapsTextures : Array[String] = [ \
 "res://assets/sprites/Maps/nonexistentMap.png"]
 var MapsNames : Array[String] = [ \
 "Qrater, Qave and Mountain", \
-"GreenMap", \
+"Quiet Meadow", \
 "RedMap", \
 "Procedural Map"]
 
